67fd15051831e943ec3a8e8c03d88df8d33740c2
Raquel Blazquez <raquel.blazquez@bosonit.com>
2023-09-19 09:21:14 +0200

    funcionalidad guard

--- src/app/auth/guards/auth.guard.ts
+++ src/app/auth/guards/auth.guard.ts
@@ -1,25 +1,38 @@
 import { Injectable } from '@angular/core';
 import {
-    ActivatedRouteSnapshot,
+  ActivatedRouteSnapshot,
   CanActivate,
   CanMatch,
   Route,
+  Router,
   RouterStateSnapshot,
   UrlSegment,
   UrlTree,
 } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
+import { AuthService } from '../services/auth.service';
 
 @Injectable({ providedIn: 'root' })
 export class AuthGuard implements CanMatch, CanActivate {
-  constructor() {}
+  constructor(private authService: AuthService, private router: Router) {}
+
+  private checkAuthStatus(): boolean | Observable<boolean> {
+    return this.authService.checkAuthentication().pipe(
+      tap( isAuthenticated => console.log('Authenticated:', isAuthenticated)),
+      tap((isAuthenticated) => {
+        if (!isAuthenticated) this.router.navigate(['./auth/login']);
+      })
+    );
+  }
+
   canMatch(
     route: Route,
     segments: UrlSegment[]
   ): boolean | Observable<boolean> {
     // console.log('Can Match');
     // console.log({ route, segments })
-    return true;
+    // return true;
+    return this.checkAuthStatus();
   }
 
   canActivate(
@@ -28,7 +41,7 @@ export class AuthGuard implements CanMatch, CanActivate {
   ): boolean | Observable<boolean> {
     // console.log('Can Activate');
     // console.log({ route, state })
-
-    return true;
+    //return true;
+    return this.checkAuthStatus();
   }
 }
